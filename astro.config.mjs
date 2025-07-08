c2231399f8fe2a7ab7dda07a182f1f8085069b5e
João Rafael Alves de Oliveira <jraoliveira.bio@gmail.com>
2025-07-08 08:37:59 -0400

    consertando links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,7 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
+// https://astro.build/config
 export default defineConfig({
-  vite: {
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, './src'),
-      },
-    },
-  },
+  site: 'https://jraoliveira-bio.github.io',
+  base: '/oficinadecientistas',
 });
