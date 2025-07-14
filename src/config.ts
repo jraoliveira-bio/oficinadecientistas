9a8f882423d8bc337291a59a87264a487a881d91
João Rafael Alves de Oliveira <jraoliveira.bio@gmail.com>
2025-07-14 08:12:53 -0400

    Videoplayer.astro, primeira versão funcional

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+// src/config.ts
+import { defineCollection, z } from 'astro:content';
+
+// Define a coleção para o curso de escrita
+const cursoEscritaCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    ordem: z.number(), // Para ordenar as aulas no menu
+    shortTitle: z.string().optional(), // Título curto para o menu (opcional)
+  }),
+});
+
+export const collections = {
+  'curso-escrita': cursoEscritaCollection,
+};
