112ce34f2d4c24a79755e0609e09e5bbe447407a
João Rafael Alves de Oliveira <jraoliveira.bio@gmail.com>
2025-09-10 12:05:01 -0400

    Versão alfa indo ao ar - revisão da primeira aula por pares

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,10 +2,11 @@
 export default new Map([
 ["src/content/curso-escrita/introducao.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcurso-escrita%2Fintroducao.mdx&astroContentModuleFlag=true")],
 ["src/content/curso-escrita/precisao.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcurso-escrita%2Fprecisao.mdx&astroContentModuleFlag=true")],
-["src/content/curso-escrita/aula01/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcurso-escrita%2Faula01%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/curso-escrita/aula02/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcurso-escrita%2Faula02%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/curso-escrita/aula04/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcurso-escrita%2Faula04%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/curso-escrita/aula01/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcurso-escrita%2Faula01%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/curso-escrita/aula03/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcurso-escrita%2Faula03%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/curso-escrita/aula04/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcurso-escrita%2Faula04%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/curso-escrita/aula05/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcurso-escrita%2Faula05%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/curso-escrita/aula06/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcurso-escrita%2Faula06%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/curso-escrita/prototype/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcurso-escrita%2Fprototype%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
