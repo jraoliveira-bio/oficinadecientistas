98e8728150a6501868e656c70831d05b6084f4eb
João Rafael Alves de Oliveira <jraoliveira.bio@gmail.com>
2026-01-30 13:38:25 -0400

    Compartimentalizando home (100% astro)

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/content/blog/2025-11-05-primeiro-post.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F2025-11-05-primeiro-post.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/2025-11-06-esboco-de-layout.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F2025-11-06-esboco-de-layout.mdx&astroContentModuleFlag=true")],
+["src/content/blog/2025-11-05-primeiro-post.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F2025-11-05-primeiro-post.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/2025-11-08-sistema-de-tags.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F2025-11-08-sistema-de-tags.mdx&astroContentModuleFlag=true")],
 ["src/content/curso-escrita/introducao.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcurso-escrita%2Fintroducao.mdx&astroContentModuleFlag=true")],
 ["src/content/curso-escrita/precisao.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcurso-escrita%2Fprecisao.mdx&astroContentModuleFlag=true")],
